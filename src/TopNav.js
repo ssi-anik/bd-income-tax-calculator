ee8974d2e14ef2e173b556eeca0eb007e8d15ba8
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-11-23 22:53:44 +0600

    how to use modal added

--- src/TopNav.js
+++ src/TopNav.js
@@ -3,10 +3,14 @@ import {Button, Modal, Nav, Navbar} from "react-bootstrap";
 import 'github-fork-ribbon-css/gh-fork-ribbon.css';
 
 export default function TopNav(props) {
-    const [show, setShow] = useState(false);
+    const [disclaimer, setDisclaimer] = useState(false);
+    const [howTo, setHowTo] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleDisclaimerClose = () => setDisclaimer(false);
+    const handleDisclaimerShow = () => setDisclaimer(true);
+
+    const handleHowToClose = () => setHowTo(false);
+    const handleHowToShow = () => setHowTo(true);
 
     const {lifetime, daily} = props;
 
@@ -16,25 +20,28 @@ export default function TopNav(props) {
             <Navbar.Toggle aria-controls="navbar-nav"/>
             <Navbar.Collapse id="navbar-nav">
                 <Nav className="mr-1">
-                    <Nav.Link className="text-warning" onClick={handleShow}>Disclaimer</Nav.Link>
+                    <Nav.Link className="text-warning" onClick={handleDisclaimerShow}>Disclaimer</Nav.Link>
                 </Nav>
-                <Nav className="mr-2 ml-2">
+                <Nav className="mr-1">
+                    <Nav.Link className="text-info" onClick={handleHowToShow}>How to use?</Nav.Link>
+                </Nav>
+                <Nav className="mr-2">
                     <Navbar.Text className="text-white">Lifetime visitor: {lifetime}</Navbar.Text>
                 </Nav>
-                <Nav className="mr-2 ml-2">
+                <Nav className="mr-2">
                     <Navbar.Text className="text-white">Daily Visitor: {daily}</Navbar.Text>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
 
-        <a target = "_blank" className = "github-fork-ribbon right-top fixed"
-           href = "https://github.com/ssi-anik/bd-income-tax-calculator"
-           rel = "noopener noreferrer"
-           data-ribbon = "Star me on GitHub" title = "Star me on GitHub">
+        <a target="_blank" className="github-fork-ribbon right-top fixed"
+           href="https://github.com/ssi-anik/bd-income-tax-calculator"
+           rel="noopener noreferrer"
+           data-ribbon="Star me on GitHub" title="Star me on GitHub">
             Star me on GitHub
         </a>
 
-        <Modal show={show} keyboard={true} animation={false} onHide={handleClose} backdrop="static">
+        <Modal show={disclaimer} keyboard={true} animation={false} onHide={handleDisclaimerClose} backdrop="static">
             <Modal.Header closeButton>
                 <Modal.Title>Disclaimer</Modal.Title>
             </Modal.Header>
@@ -69,7 +76,77 @@ export default function TopNav(props) {
                 </ul>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
+                <Button variant="secondary" onClick={handleDisclaimerClose}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
+        <Modal show={howTo} keyboard={true} animation={false} onHide={handleHowToClose} backdrop="static">
+            <Modal.Header closeButton>
+                <Modal.Title>How to use?</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p className="text-success">
+                    All the calculations are done in front-end/browser. No data is being sent to the backend or server.
+                    If you reload the page, all the data will be gone from the page. And all the calculations are done
+                    in the realtime. So, you can change the values.
+                </p>
+                <hr/>
+                <p className="text-info">
+                    If you're a developer, then don't forget to click the star button on this repository by clicking
+                    "Star me on Github"
+                </p>
+                <hr/>
+                <ul>
+                    <li>Select if you're male, female or have any other privilege</li>
+                    <li>Select your age group.</li>
+                    <li>Input the minimum tax amount applicable for your area.</li>
+                </ul>
+                <hr/>
+                <ul>
+                    <li>Enter the company name you worked for.</li>
+                    <li>Number of months you worked there. Default is 12.</li>
+                    <li>Number of festivals you got within those months. Default is 2.</li>
+                    <li>
+                        If you want to calculate per months basis, select "Calculate from Monthly".
+                        Otherwise, you if you have your annual salary certificate, you can calculate from there.
+                    </li>
+                </ul>
+                <hr/>
+                <ul>
+                    <li>Enter the salary breakdown as specified in the fields.</li>
+                    <li>
+                        Festival bonus are multiplied by the no of festivals specified.
+                    </li>
+                    <li>Other taxable bonus are the
+                        total amount of taxable bonus you received like "Leave encashment", "PF"
+                    </li>
+                    <li>Press Calculate button or reset if you want to calculate or reset.</li>
+                    <li>You can remove existing entry by clicking the remove button under the listing table.</li>
+                    <li>
+                        You can use as many companies as you want. In case you've got an increment in the mid of a FY.
+                    </li>
+                </ul>
+                <hr/>
+                <ul>
+                    <li>
+                        AIT/Advance Income Tax is the tax deducted by the office per month basis.
+                        Insert a value if they did.
+                    </li>
+                    <li>
+                        Allowable investments are the investments that are counted as investment.
+                        Enter the sum of allowed investments.
+                    </li>
+                </ul>
+                <hr/>
+                <p className="text-info">
+                    You'll then be able to see the available information as well as the tax
+                    breakdown.
+                </p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleHowToClose}>
                     Close
                 </Button>
             </Modal.Footer>
